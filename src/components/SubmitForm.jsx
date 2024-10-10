ae9572a3e562b9d137e71c5c5434441cbb101141
unknown <ITRYN@DESKTOP-063FE12.in-sekuritas.com>
2024-10-10 14:09:48 +0700

    committt

--- src/components/SubmitForm.jsx
+++ src/components/SubmitForm.jsx
@@ -7,53 +7,60 @@ import toast from "react-hot-toast";
 export function SubmitDemo() {
   const handleSubmit = (e) => {
     e.preventDefault();
-  
-    const form = document.getElementById('form-submit');
+
+    const form = document.getElementById("form-submit");
+    const filename = form.filename.value.trim();
     const file = form.file.files[0]; // Retrieve the uploaded file
-    
+
     if (!file) {
-      console.error("No file selected");
+      toast.error("Make sure to select file");
       return;
     }
-  
+
+    if (!filename) {
+      toast.error("Make sure to fill in input");
+      return;
+    }
+
     const url =
       "https://script.google.com/macros/s/AKfycby_dDBZAlviHnr4ATbL7qrqdLZG94tHL0RTQyTMYcGa6axV53nL-M5cAbMyHGDl9DAx/exec";
-    
+
     const qs = new URLSearchParams({
       filename: form.filename.value || file.name, // Filename from input or the file name
       mimeType: file.type, // File MIME type
     });
-  
+
     const reader = new FileReader();
-    
+
     reader.onload = (f) => {
-      fetch(`${url}?${qs}`, {
-        method: "POST",
-        mode: "no-cors",
-        headers: {
-          "Content-Type": "application/json",
+      fetch(
+        `${url}?${qs}`,
+        {
+          method: "POST",
+          mode: "no-cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify([...new Int8Array(f.target.result)]), // Upload file data as byte array
         },
-        body: JSON.stringify([...new Int8Array(f.target.result)]), // Upload file data as byte array
-      }
-      ,form.reset()
-    )
+        form.reset(),
+      )
         .then((res) => res.json()) // Properly call .json()
         .then((data) => {
-          toast.success('File Successfully Uploaded!')
+          toast.success("File Successfully Uploaded!");
         })
         .catch((error) => toast.error(error));
     };
-  
+
     reader.readAsArrayBuffer(file); // Read file as array buffer
   };
-  
 
   return (
-    <div className="max-w-md w-full mx-auto rounded-none md:rounded-2xl p-4 md:p-8 shadow-input bg-white dark:bg-black">
-      <h2 className="font-bold text-xl text-neutral-800 dark:text-neutral-200">
+    <div className="mx-auto w-full max-w-md bg-white p-4 shadow-input dark:bg-black sm:rounded-lg md:rounded-2xl md:p-8">
+      <h2 className="text-xl font-bold text-neutral-800 dark:text-neutral-200">
         Ryan's Drive Uploader Express
       </h2>
-      <p className="text-neutral-600 text-sm max-w-sm mt-2 dark:text-neutral-300">
+      <p className="mt-2 max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
         Drop file here to upload it to Google Drive
       </p>
       <form className="my-6" onSubmit={handleSubmit} id="form-submit">
@@ -72,14 +79,14 @@ export function SubmitDemo() {
         </LabelInputContainer>
 
         <button
-          className="bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+          className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
           type="submit"
         >
           Upload &rarr;
           <BottomGradient />
         </button>
 
-        <div className="bg-gradient-to-r from-transparent via-neutral-300 dark:via-neutral-700 to-transparent my-8 h-[1px] w-full" />
+        <div className="my-2 h-[1px] w-full bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-neutral-700" />
       </form>
     </div>
   );
@@ -88,15 +95,15 @@ export function SubmitDemo() {
 const BottomGradient = () => {
   return (
     <>
-      <span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
-      <span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+      <span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
+      <span className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100" />
     </>
   );
 };
 
 const LabelInputContainer = ({ children, className }) => {
   return (
-    <div className={cn("flex flex-col space-y-2 w-full", className)}>
+    <div className={cn("flex w-full flex-col space-y-2", className)}>
       {children}
     </div>
   );
