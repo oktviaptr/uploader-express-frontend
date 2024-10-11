04efaf7cadd1f6fc913ea6f4ca80d9193cbcce5d
unknown <ryanananda.april@gmail.com>
2024-10-11 16:57:08 +0700

    adding backend

--- app.cjs
+++ app.cjs
@@ -1,21 +1,31 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
+const FrontEnd = require('./src/api/frontEnd')
 
 app.use((req, res, next) => {
-    const allowedDNS = '192.168.1.1'; //  allowed domain
+    const allowedDNS = 'localhost' || 'in-sekuritas.com'; // Set Rules for Access
     const host = req.get('host');
     
     if (host.includes(allowedDNS)) {
         next(); 
     } else {
-        res.status(403).send('Forbidden: Invalid DNS'); // Deny access
+        res.status(403).send('Forbidden Access: unknown user, only author can access this website!'); // Deny access
     }
 });
 
+// app.use(cors({ origin: 'http://http://localhost:5173' }));
+
+
+// Run and render UI to /
 app.get('/', (req, res) => {
-    res.send('Welcome to the website!');
+    res.send('Granted Access to the website!');
+    
 });
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.use('/', FrontEnd)
+
+app.listen(PORT, function (err) {
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
 });
---
+++ src/api/frontEnd.js
@@ -0,0 +1,6 @@
+const router = require('express').Router()
+const { request } = require('http')
+const path = require('path')
+
+
+const toMainHome = (request)
